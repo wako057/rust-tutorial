5a627027421932f861b642f3ed831e1a0093d06d
Gregory Gerard <wako057@gmail.com>
2019-09-11 13:41:02 +0200

    feat(chapter3-data-flow): add chapter 3.5

---
+++ chapter3/data_flow/src/main.rs
@@ -0,0 +1,83 @@
+fn main() {
+    let number = 7;
+
+    println!("=== Presentation du if ===");
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+
+    let number = 6;
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+    let number = if condition {
+        5
+    } else {
+        6
+    };
+//    KO le type doit etre identique
+//    let number = if condition {
+//        5
+//    } else {
+//        "six"
+//    };
+    println!("The value of number is: {}", number);
+
+    println!("=== Presentation de la boucle loop ===");
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {}", result);
+
+    println!("=== Presentation de la boucle while ===");
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+
+    println!("=== Presentation de la boucle for avec iterateur ===");
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+}
