214030d5e117ee31936046617ff9c726fb645888
Gregory GERARD <wako057@gmail.com>
2020-10-27 20:16:51 +0100

    feat: add immutable error in comment

--- chapter4/references/src/main.rs
+++ chapter4/references/src/main.rs
@@ -7,11 +7,15 @@ fn main() {
 
     let mut s = String::from("hello");
     change(&mut s);
+    // KO on ne peut pas changer une variable meme par reference si elle est immuable
+    //  change_fail(&s);
+
 //    KO On ne peut avoir qu'une seul reference mutable par entite
 //    let mut s = String::from("hello");
 //    let r1 = &mut s;
 //    let r2 = &mut s;
 //    println!("{}, {}", r1, r2);
+    println!("{}", s);
 }
 
 fn calculate_length(s: &String) -> usize {
@@ -21,3 +25,7 @@ fn calculate_length(s: &String) -> usize {
 fn change(some_string: &mut String) {
     some_string.push_str(", world");
 }
+
+// fn change_fail(some_string: &String) {
+//     some_string.push_str(", world");
+// }
